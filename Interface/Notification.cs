61458be246a71d73d8b1d43bd95444b0a6d8309c
Aram Baghdasarian <aram99551@gmail.com>
2021-09-29 17:27:50 +0400

    Add project files.

---
+++ Interface/Notification.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    public class Notification
+    {
+        private readonly  IMessage _message;
+        public Notification(IMessage message)
+        {
+            _message = message;
+        }
+        public void DoNotify()
+        {
+            _message.SendMessage();
+        }
+    }
+}
---
+++ Interface/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Interface
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Notification notificationEmail = new Notification(new Email());
+            Notification notificationDrone = new Notification(new Drone());
+            Notification notificationSMS = new Notification(new SMS());
+            Notification notificationEnvelop = new Notification(new Envelop());
+            Notification notificationMMS = new Notification(new MMS());
+
+            List<Notification> notifications = new List<Notification>();
+
+            notifications.Add(notificationEmail);
+            notifications.Add(notificationDrone);
+            notifications.Add(notificationSMS);
+            notifications.Add(notificationEnvelop);
+            notifications.Add(notificationMMS);
+
+            for (int i = 0; i < notifications.Count; i++)
+            {
+                notifications[i].DoNotify();
+            }
+
+        }
+    }
+}
